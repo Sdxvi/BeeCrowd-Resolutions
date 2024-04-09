a2f56ea4f96dbbb76e8808c35e6a64aa80bdb66d
Vinicius Giacomelli <viniciuscamargogiacomelli55@gmail.com>
2024-04-09 18:43:32 -0300

    Resolucao Hello World! - exercicio BEE 1000
    - Inicializando Repositorio
    - Resolvido nas linguagens Java, Python, JavaScript e C
    - Primeiro exercicio da dificuldade iniciante do BeeCrowd

---
+++ Iniciante/Java/BEE_1000.java
@@ -0,0 +1,23 @@
+/* 
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". 
+Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+Entrada
+Este problema não possui nenhuma entrada.
+
+Saída
+Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha.
+*/
+
+import java.io.IOException;
+ 
+public class BEE_1000 { //Para utilizar este código no BEECROWD a public class deve ser "Main"
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.println("Hello World!");
+ 
+    }
+ 
+}
+
